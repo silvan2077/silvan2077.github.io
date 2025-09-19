b39d1a12470db617c43289a6f42776d287219058
silvan2077 <18669513992@163.com>
2025-09-19 21:53:05 +0800

    Site updated: 2025-09-19 21:53:04

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = '👀你还回来吗~~~';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = '~\(≧▽≦)/~欢迎回来！';
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 2000);
-  }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="👀你还回来吗~~~",clearTimeout(titleTime)):(document.title="~(≧▽≦)/~欢迎回来！",titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
